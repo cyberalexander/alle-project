f1295a11224ac5ceb3c2d9961b1d9ef937b9ebb4
CyberAlexander <festivdom@gmail.com>
2016-09-15 23:01:23 +0300

    commit 15.09.2016 23:01

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 /**
  * Created by alexanderleonovich on 11.09.16.
  */
-function onFocusForm() {
-    console.log('ASDSDASDASDASD');
-    var field = document.getElementById('name');
-    console.log(field);
+function showMenu() {
+    var elementVar = document.getElementById("mobile-nav-list");
+    console.log("display menu before " + elementVar.style.display);
+    if(elementVar.style.display == ""){
+        elementVar.style.display = "block";
+        elementVar.style.zIndex = 1000;
+    }else{
+        elementVar.style.display = '';
+        elementVar.style.zIndex = 0;
+    }
+    console.log("display menu after " + elementVar.style.display);
+}
 
-    if (this.value == this.defaultValue) {
-        this.value = '';
-    }
-}
-function onBlurForm() {
-    if(this.value == ''){
-        this.value = this.defaultValue;
-    }
-}
+
+
+
+// function onFocusForm() {
+//     console.log('ASDSDASDASDASD');
+//     var field = document.getElementById('name');
+//     console.log(field);
+//
+//     if (this.value == this.defaultValue) {
+//         this.value = '';
+//     }
+// }
+// function onBlurForm() {
+//     if(this.value == ''){
+//         this.value = this.defaultValue;
+//     }
+// }
