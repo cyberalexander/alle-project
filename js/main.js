179dc14a2050ff43b54dab5ab407d82bad0013d6
cyberalexander <taurum13@gmail.com>
2016-09-16 19:01:48 +0300

    Commit 16.09.2016 19:01

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
 /**
  * Created by alexanderleonovich on 11.09.16.
  */
-function showMenu() {
-    var elementVar = document.getElementById("mobile-nav-list");
-    console.log("display menu before " + elementVar.style.display);
-    if(elementVar.style.display == ""){
-        elementVar.style.display = "block";
-        elementVar.style.zIndex = 1000;
-    }else{
-        elementVar.style.display = '';
-        elementVar.style.zIndex = 0;
+// function showMenu() {
+//     var elementVar = document.getElementById("mobile-nav-list");
+//     console.log("display menu before " + elementVar.style.display);
+//     if(elementVar.style.display == ""){
+//         elementVar.style.display = "block";
+//         elementVar.style.zIndex = 1000;
+//     }else{
+//         elementVar.style.display = '';
+//         elementVar.style.zIndex = 0;
+//     }
+//     console.log("display menu after " + elementVar.style.display);
+// }
+
+
+function onFocusForm(inputElement) {
+    var value = inputElement.value;
+    var defaultValue = inputElement.defaultValue;
+    console.log('inputElement = ' + inputElement);
+    console.log('value = ' + value);
+    console.log('defaultValue = ' + defaultValue);
+    if (value == defaultValue) {
+        inputElement.value = '';
     }
-    console.log("display menu after " + elementVar.style.display);
 }
 
-
-
-
-// function onFocusForm() {
-//     console.log('ASDSDASDASDASD');
-//     var field = document.getElementById('name');
-//     console.log(field);
-//
-//     if (this.value == this.defaultValue) {
-//         this.value = '';
-//     }
-// }
-// function onBlurForm() {
-//     if(this.value == ''){
-//         this.value = this.defaultValue;
-//     }
-// }
+function onBlurForm(inputElement) {
+    var value = inputElement.value;
+    var defaultValue = inputElement.defaultValue;
+    console.log('inputElement = ' + inputElement);
+    console.log('value = ' + value);
+    console.log('defaultValue = ' + defaultValue);
+    if (value == '') {
+        inputElement.value = defaultValue;
+    }
+}
